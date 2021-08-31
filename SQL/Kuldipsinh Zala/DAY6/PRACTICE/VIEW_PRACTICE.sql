042648bbd1e8329d8295961c5bfc4e9f3c3409d8
zalakuldip740 <zalakuldipsinh740@gmail.com>
2021-08-31 21:21:35 +0530

    REFS : #851 AND FIXES #851 SQL DAY7 ASSIGNMENT,PRACTICE AND ADDITIONAL FILES

--- SQL/Kuldipsinh Zala/DAY6/PRACTICE/VIEW_PRACTICE.sql
+++ SQL/Kuldipsinh Zala/DAY6/PRACTICE/VIEW_PRACTICE.sql
@@ -18,7 +18,12 @@
 	
 	DROP VIEW DEMO2;
 	
+	      -- UPDATE & REPLACE VIEW (NOT SUPPORTED IN SQLITE)
 	
 	CREATE OR REPLACE VIEW DEMO_VIEW
-	AS                                                -- ERROR
+	AS                                                                                     -- ERROR
 	SELECT FirstName,LastName FROM Employees;         -- SQLITE VIEW IS READ ONLY
+	
+	
+	
+	UPDATE DEMO_VIEW SET Salary=25000 WHERE EmployeeID=109;   -- ERROR:cannot modify DEMO_VIEW because it is a view
